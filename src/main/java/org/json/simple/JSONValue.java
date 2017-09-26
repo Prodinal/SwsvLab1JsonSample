9fe85311503cbcec050eaa8bacad8c9a05c063ff
Rapcsák Péter <rapcsak.peter95@gmail.com>
2017-09-26 18:26:37 +0200

    Removed commented out codeline

--- src/main/java/org/json/simple/JSONValue.java
+++ src/main/java/org/json/simple/JSONValue.java
@@ -10,7 +10,6 @@ import java.io.StringReader;
 import java.io.StringWriter;
 import java.io.Writer;
 import java.util.Collection;
-// import java.util.List;
 import java.util.Map;
 
 import org.json.simple.parser.JSONParser;
